451fe2776e765b5339164cf4f5700e516c570cc3
Frank Marchese <133362490+frankcrafter@users.noreply.github.com>
2025-11-06 19:31:28 +0100

    Add files via upload

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -11,6 +11,27 @@ return {
 	},
 
 	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "" },
+				topdelete = { text = "" },
+				changedelete = { text = "▎" },
+				untracked = { text = "▎" },
+			},
+			signs_staged = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "" },
+				topdelete = { text = "" },
+				changedelete = { text = "▎" },
+			},
+		},
+	},
+
+	{
 		"iamcco/markdown-preview.nvim",
 		lazy = false,
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -17,27 +17,6 @@ return {
 	},
 
 	{
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "▎" },
-				change = { text = "▎" },
-				delete = { text = "" },
-				topdelete = { text = "" },
-				changedelete = { text = "▎" },
-				untracked = { text = "▎" },
-			},
-			signs_staged = {
-				add = { text = "▎" },
-				change = { text = "▎" },
-				delete = { text = "" },
-				topdelete = { text = "" },
-				changedelete = { text = "▎" },
-			},
-		},
-	},
-
-	{
 		"brenoprata10/nvim-highlight-colors",
 		opts = {
 			render = "background",
